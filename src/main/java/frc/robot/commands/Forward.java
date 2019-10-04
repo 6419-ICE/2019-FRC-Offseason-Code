f5c29a5f29a427f082ad5b4cf4e92356215bc774
VeloticsTechnologies <hcsgeek@gmail.com>
2019-10-04 11:36:25 -0500

    Add gyro input

--- src/main/java/frc/robot/commands/Forward.java
+++ src/main/java/frc/robot/commands/Forward.java
@@ -1,5 +1,7 @@
 package frc.robot.commands;
 
+import com.analog.adis16448.frc.ADIS16448_IMU;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.Timer;
 import frc.robot.Robot;
@@ -8,7 +10,10 @@ import frc.robot.Robot;
  * Move Forward to Cargo Ship
  */
 public class Forward extends Command{
-   Timer timer = new Timer();
+   private final Timer timer = new Timer();
+   private final ADIS16448_IMU imu = new ADIS16448_IMU();
+   private static final double kAngleSetpoint = 0.0;
+   private static final double kP = 0.005; // propotional turning constant
 
     @Override
     protected void initialize() {
@@ -19,7 +24,8 @@ public class Forward extends Command{
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-      Robot.drivetrain.drive(0.5, 0.5);
+        double turningValue = (kAngleSetpoint - imu.getAngle()) * kP;
+        Robot.drivetrain.drive(0.5 - turningValue, 0.5 + turningValue);
     }
 
     // Make this return true when this Command no longer needs to run execute()
