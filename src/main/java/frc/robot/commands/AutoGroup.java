9fe5431f8195fe138f0ac6b9fe91cd5b8c36eb1f
PradyumnaDahal <pradyumnadahal2003@gmail.com>
2019-10-03 20:23:44 -0500

    AutoGroup

--- src/main/java/frc/robot/commands/AutoGroup.java
+++ src/main/java/frc/robot/commands/AutoGroup.java
@@ -7,10 +7,14 @@ import frc.robot.Robot;
  * Handle the Drivetrain
  */
 public class AutoGroup extends CommandGroup{
+    private String name;
 
-    public AutoGroup() {
+    public AutoGroup(String n) {
+        name = n;
+
         addSequential(new Forward());
         addSequential(new TurnLeft());
+        addSequential(new Forward());
         addSequential(new DeliverHatchPanel());
     }
 
