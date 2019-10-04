bcc22c3b3f67f169a1d00595c854ac08745c1774
Nerdium <34074963+Nerdium@users.noreply.github.com>
2019-10-04 09:14:05 -0500

    Update Forward.java

--- src/main/java/frc/robot/commands/Forward.java
+++ src/main/java/frc/robot/commands/Forward.java
@@ -7,26 +7,30 @@ import frc.robot.Robot;
  * Move Forward to Cargo Ship
  */
 public class Forward extends Command{
-   
+   Timer timer = new Timer();
+
     @Override
     protected void initialize() {
         Robot.drivetrain.drive(0, 0); // Don't move on init
+        timer.start();
     }
 
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+      Robot.drivetrain.drive(0.5, 0.5);
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return timer.get() < 2.0;
     }
 
     // Called once after isFinished returns true
     @Override
     protected void end() {
+      Robot.drivetrain.stop();
     }
 
     // Called when another command which requires one or more of the same
