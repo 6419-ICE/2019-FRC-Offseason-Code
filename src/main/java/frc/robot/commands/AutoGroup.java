1e4173c13f1b53ffe6759f46054ba8c6e5cbda4e
PradyumnaDahal <pradyumnadahal2003@gmail.com>
2019-10-10 19:15:45 -0500

    AutoGroup Edits

--- src/main/java/frc/robot/commands/AutoGroup.java
+++ src/main/java/frc/robot/commands/AutoGroup.java
@@ -4,7 +4,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 import frc.robot.Robot;
 
 /**
- * Autonomous Command Group - Primary Auto
+ * Handle the Drivetrain
  */
 public class AutoGroup extends CommandGroup{
     private String name;
@@ -16,33 +16,5 @@ public class AutoGroup extends CommandGroup{
         addSequential(new TurnLeft());
         addSequential(new Forward());
         addSequential(new DeliverHatchPanel());
-    }
-
-    @Override
-    protected void initialize() {
-        Robot.drivetrain.drive(0, 0); // Don't move on init
-        System.out.println(name + "has started!");
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    @Override
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    @Override
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    @Override
-    protected void interrupted() {
     }
 }
