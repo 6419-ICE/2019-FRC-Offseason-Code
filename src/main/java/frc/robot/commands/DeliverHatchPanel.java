1fb66c9c5b7a2ef2c3d3101eb2041dad0f98b8b5
Fdgttygt5 <37253061+Fdgttygt5@users.noreply.github.com>
2019-10-29 15:02:22 -0500

    Basic Outline for command

--- src/main/java/frc/robot/commands/DeliverHatchPanel.java
+++ src/main/java/frc/robot/commands/DeliverHatchPanel.java
@@ -1,29 +1,44 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.Timer;
 // import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.Robot;
+import frc.robot.RobotMap;
 
 /**
  * Deliver Hatch Panel to Cargo Ship
  */
 public class DeliverHatchPanel extends Command{
    
+   private Timer timer = new Timer();
+   private boolean isDone = false;
+   
     @Override
     protected void initialize() {
         Robot.drivetrain.drive(0, 0); // Don't move on init
+        Robot.arm.armMotor(0);
+        Robot.arm.hookSolenoid(Value.kOff);
         requires(Robot.arm);
     }
 
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+       if(timer.get() < 1){
+          Robot.arm.armMotor(1);
+       }else if(timer.get() > 1){
+          robot.arm.hookSolenoid(Value.kReverse);
+       } else if(timer.get() > 1.3){
+          isDone = true;
+       }
     }
 
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
-        return false;
+        return isDone;
     }
 
     // Called once after isFinished returns true
