cdd4b3daccf5818eb3c01e88d2e3c6c5efe376d1
Fdgttygt5 <37253061+Fdgttygt5@users.noreply.github.com>
2019-10-30 05:13:06 -0500

    First Attempt at Limelight assist

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,9 +17,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import frc.robot.commands.*;
 import frc.robot.subsystems.*;
 import frc.robot.subsystems.Limelight.LightMode;
-
-
-// import edu.wpi.first.networktables.*; //PD
+import edu.wpi.first.networktables.*; 
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -149,31 +147,29 @@ public class Robot extends TimedRobot {
   public void teleopPeriodic() {
     buttonHandler.checkButtons();
     Scheduler.getInstance().run();
-
-    /* PD
     Update_Limelight_Tracking();
-        double steer = m_Controller.getX(Hand.kRight);
-        double drive = -m_Controller.getY(Hand.kLeft);
-        boolean auto = m_Controller.getAButton();
-        steer *= 0.70;
-        drive *= 0.70;
+        boolean auto = this.m_oi.limelightButton().get();
+
         if (auto)
         {
           if (m_LimelightHasValidTarget)
           {
-                m_Drive.arcadeDrive(m_LimelightDriveCommand,m_LimelightSteerCommand);
+                if (HandleDriveTrain().isRunning){
+                  HandleDriveTrain().cancel(); 
+                }
+                this.drivetrain.drive(m_LimelightDriveCommand, m_LimelightDriveCommand);
           }
           else
           {
-                m_Drive.arcadeDrive(0.0,0.0);
+                this.drivetrain.drive(0, 0);
           }
         }
         else
         {
-          m_Drive.arcadeDrive(drive,steer);
+          HandleDriveTrain().start();
         }
   }
-  */
+  
 
   }
 
@@ -182,11 +178,10 @@ public class Robot extends TimedRobot {
     Scheduler.getInstance().run();
   }
   
-  /* public static void Update_Limelight_Tracking() { // PD
+  public static void Update_Limelight_Tracking() { // PD
     // Limelight control
     // These numbers must be tuned for your Robot!  Be careful!
-    final double STEER_K = 0.03;                    // how hard to turn toward the target
-    final double DRIVE_K = 0.26;                    // how hard to drive fwd toward the target
+    final double DRIVE_K = 0.3;                    // how hard to drive fwd toward the target
     final double DESIRED_TARGET_AREA = 13.0;        // Area of the target when the robot reaches the wall
     final double MAX_DRIVE = 0.7;                   // Simple speed limit so we don't drive too fast
     double tv = NetworkTableInstance.getDefault().getTable("limelight").getEntry("tv").getDouble(0);
@@ -195,18 +190,20 @@ public class Robot extends TimedRobot {
     double ta = NetworkTableInstance.getDefault().getTable("limelight").getEntry("ta").getDouble(0);
     boolean m_LimelightHasValidTarget;
     double m_LimelightDriveCommand;
-    double m_LimelightSteerCommand;
+    Command m_turnCommand;
+    
     if (tv < 1.0)
     {
       m_LimelightHasValidTarget = false;
       m_LimelightDriveCommand = 0.0;
-      m_LimelightSteerCommand = 0.0;
       return;
     }
     m_LimelightHasValidTarget = true;
     // Start with proportional steering
-    double steer_cmd = tx * STEER_K;
-    m_LimelightSteerCommand = steer_cmd;
+    if(tx < 2 || tx > 2){
+      m_turnCommand = new TurnToHeading(tx)
+      m_turnCommand.start();
+    }
     // try to drive forward until the target area reaches our desired area
     double drive_cmd = (DESIRED_TARGET_AREA - ta) * DRIVE_K;
     // don't let the robot drive too fast into the goal
@@ -216,5 +213,5 @@ public class Robot extends TimedRobot {
     }
     m_LimelightDriveCommand = drive_cmd;
   }
-  */
+  
 }
